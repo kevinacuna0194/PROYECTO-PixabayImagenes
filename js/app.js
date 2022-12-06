adb7418fc69747c8e42f58fcf78b611bf76a9ad4
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2022-12-06 18:31:15 -0300

    Generar un Pagindor con un Generador

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const resultado = document.querySelector('#resultado');
 
 const registrosPorPagina = 40;
 let totalPaginas;
+let iterador;
 
 window.onload = () => {
     formulario.addEventListener('submit', validarFormulario)
@@ -21,54 +22,6 @@ function validarFormulario(e) {
 
     /** Cuando pasemos la validación Buscaremos la imágenes en Pixabay */
     buscarImagenes(terminoBusqueda);
-}
-
-function buscarImagenes(termino) {
-    /** Puedes ver que tenemos este aquí está a pie en específico. Requiere que le pasemos una piqui. De esa forma identifican cuántas consultas estás haciendo  */
-    const key = '24423264-389224fa4f79c639625c30ad4';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
-
-    // console.log(url);
-
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
-            /** console.log(resultado);  {total: 3808, totalHits: 500, hits: Array(100)} */
-            totalPaginas = calcularPaginas(resultado.totalHits);
-            mostrarImagenes(resultado.hits);
-        })
-}
-
-function mostrarImagenes(imagenes) {
-    console.log(imagenes); /** (20) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}] */
-    limpiarHTML();
-
-    /** Iterar sobre el arreglo de imagenes y construir el HTML */
-
-    imagenes.forEach(imagen => {
-
-        const { previewURL, likes, views, largeImageURL } = imagen;
-
-        resultado.innerHTML += `
-            <div class="w-1/2 md:w-1/3 lg:w-1/4 mb-4 p-3">
-                <div class="bg-white ">
-                    <img class="w-full" src=${previewURL} alt={tags} />
-                    <div class="p-4">
-                        <p class="card-text">${likes} Me Gusta</p>
-                        <p class="card-text">${views} Vistas </p>
-        
-                        <a href=${largeImageURL} 
-                        rel="noopener noreferrer" 
-                        target="_blank" class="bg-blue-800 w-full p-1 block mt-5 rounded text-center font-bold uppercase hover:bg-blue-500 text-white">Ver Imagen</a>
-                    </div>
-                </div>
-            </div>
-            `;
-    });
-}
-
-function calcularPaginas(total) {
-    return parseInt(Math.ceil(total / registrosPorPagina));
 }
 
 function mostrarAlerta(mensaje) {
@@ -93,8 +46,68 @@ function mostrarAlerta(mensaje) {
     }
 }
 
-function limpiarHTML() {
+function buscarImagenes(termino) {
+    /** Puedes ver que tenemos este aquí está a pie en específico. Requiere que le pasemos una piqui. De esa forma identifican cuántas consultas estás haciendo  */
+    const key = '24423264-389224fa4f79c639625c30ad4';
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
+
+    // console.log(url);
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            /** console.log(resultado);  {total: 3808, totalHits: 500, hits: Array(100)} */
+            totalPaginas = calcularPaginas(resultado.totalHits);
+            mostrarImagenes(resultado.hits);
+        })
+}
+
+/** Generador que va a registrar la cantidad de elementos de acuerod a las páginas */
+function* crearPaginador(total) {
+    console.log(total);
+    for (let i = 1; i <= total; i++) {
+        yield i;
+    }
+}
+
+function calcularPaginas(total) {
+    return parseInt(Math.ceil(total / registrosPorPagina));
+}
+
+function mostrarImagenes(imagenes) {
+    /** console.log(imagenes);  (20) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}] */
+
     while (resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
+
+    /** Iterar sobre el arreglo de imagenes y construir el HTML */
+
+    imagenes.forEach(imagen => {
+
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 mb-4 p-3">
+                <div class="bg-white ">
+                    <img class="w-full" src=${previewURL} alt={tags} />
+                    <div class="p-4">
+                        <p class="card-text">${likes} Me Gusta</p>
+                        <p class="card-text">${views} Vistas </p>
+        
+                        <a href=${largeImageURL} 
+                        rel="noopener noreferrer" 
+                        target="_blank" class="bg-blue-800 w-full p-1 block mt-5 rounded text-center font-bold uppercase hover:bg-blue-500 text-white">Ver Imagen</a>
+                    </div>
+                </div>
+            </div>
+            `;
+    });
+
+    imprimirPaginador()
+}
+
+function imprimirPaginador() {
+    iterador = crearPaginador(totalPaginas);
+    console.log(iterador.next());
 }
