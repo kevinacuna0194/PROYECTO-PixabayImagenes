c0c312346604095aa021e3c91af98a354b0f4b4b
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2022-12-07 12:12:11 -0300

    Navegar por las páginas

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const paginacionDiv = document.querySelector('#paginacion');
 const registrosPorPagina = 40;
 let totalPaginas;
 let iterador;
+let paginaActual = 1;
 
 window.onload = () => {
     formulario.addEventListener('submit', validarFormulario)
@@ -22,7 +23,7 @@ function validarFormulario(e) {
     }
 
     /** Cuando pasemos la validación Buscaremos la imágenes en Pixabay */
-    buscarImagenes(terminoBusqueda);
+    buscarImagenes();
 }
 
 function mostrarAlerta(mensaje) {
@@ -47,10 +48,13 @@ function mostrarAlerta(mensaje) {
     }
 }
 
-function buscarImagenes(termino) {
-    /** Puedes ver que tenemos este aquí está a pie en específico. Requiere que le pasemos una piqui. De esa forma identifican cuántas consultas estás haciendo  */
+function buscarImagenes() {
+
+    const termino = document.querySelector('#termino').value;
+    
+    /** Requiere que le pasemos una API Key */
     const key = '24423264-389224fa4f79c639625c30ad4';
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
     // console.log(url);
 
@@ -63,9 +67,11 @@ function buscarImagenes(termino) {
         })
 }
 
-/** Generador que va a registrar la cantidad de elementos de acuerod a las páginas */
+/** Recuerda el generador, va a permitirte iterar sobre todos los registros y te va a decir cuando ha llegado al final. Por eso estamos utilizando un generador. */
 function* crearPaginador(total) {
-    console.log(`Páginas: ${total}`);
+
+    // console.log(`Páginas: ${total}`);
+
     for (let i = 1; i <= total; i++) {
         yield i;
     }
@@ -114,6 +120,7 @@ function mostrarImagenes(imagenes) {
     imprimirPaginador()
 }
 
+/** imprimirPaginador() va a ir generando el HTML mientras no hayamos llegado al final del generador. */
 function imprimirPaginador() {
     iterador = crearPaginador(totalPaginas);
     // console.log(iterador.next());
@@ -128,7 +135,17 @@ function imprimirPaginador() {
         boton.href = '#';
         boton.dataset.pagina = value;
         boton.textContent = value;
-        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'uppercase', 'rounded');
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-4', 'rounded');
+
+        boton.onclick = () => {
+
+            paginaActual = value;
+
+            // console.log(paginaActual)
+
+            /** Consultar nuevamnete la API con un número de página diferente */
+            buscarImagenes();
+        }
 
         paginacionDiv.appendChild(boton);
     }
